b3a80ae0e9afc01298ba33bedf6db9cdb119aa65
Kishan <kishan@geekonomy.in>
2025-10-14 16:50:53 +0530

    banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Banner from "@/components/home/Banner";
+import Solutionize from "@/components/home/Solutionize";
 import InfiniteScroll from "@/components/infinteScroll";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold flex justify-center items-center h-screen">Home</h1>
+      <Banner />
+      {/* <Solutionize /> */}
       <section className="py-20 bg-black">
               <div>
                 <InfiniteScroll/>
