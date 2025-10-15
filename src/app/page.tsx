1779fb18ddf5f165ba96c8d63ed0261229add911
roshan-geek <roshan@geekonomy.in>
2025-10-15 13:43:06 +0530

    Add Expertise Section to Home page and update imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import InfiniteScroll from "@/components/infinteScroll";
 import ExpertiseData from "@/components/home/Expertise";
 import InspirationStation from "@/components/sections/InspirationStation";
 import TerrificSection from "@/components/sections/TerrificSection";
+import { digitalMarketing } from "@/components/data/expertiseSections ";
+import ExpertiseSection from "@/components/home/Expertise";
 
 
 export default function Home() {
@@ -16,6 +18,7 @@ export default function Home() {
                 <InfiniteScroll/>
               </div>
             </section>
+            <ExpertiseSection />
             <TerrificSection />
 
     <InspirationStation/>
