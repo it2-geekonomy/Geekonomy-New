be627742e186caf9d798b85b97df7c8ac6fce4fb
gopesh <gopesh@geekonomy.in>
2025-10-16 11:26:12 +0530

    Refactor HeroSection to GetInTouch component: Update imports and component usage across multiple pages for consistency and clarity.

--- src/app/branding-solutions/page.tsx
+++ src/app/branding-solutions/page.tsx
@@ -2,7 +2,7 @@ import Banner from "@/components//branding/banner";
 import ExpertiseData from "@/components/sections/ExpertiseData";
 import { brandingSolutions } from "../../app/../components/data/expertiseSections ";
 import VideoSection from "@/components/branding/vedio";
-import HeroSection from "@/components/common/getintouch";
+import GetInTouch from "@/components/common/getintouch";
 export default function Home() {
   return (  
        <div className="flex flex-col">
@@ -12,7 +12,7 @@ export default function Home() {
         src="/expertise/Branding page V4.mp4"
         poster="/images/video-poster.webp"
       />
-       <HeroSection
+       <GetInTouch
       text="We specialize in creating cohesive brand identities that resonate with your audience. From
 logo design to brand strategy, we work closely with you to bring your vision to life."
       buttonText="Contact Us"
