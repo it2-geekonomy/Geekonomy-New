65ad7ffc94f6b00dbf7fa343546e3c656721a724
Pooja Acharya <acharyapooja97@gmail.com>
2025-10-16 17:12:16 +0530

    updated two data in inspiration component

---
+++ src/components/inspirationdata/inspirationSlides.tsx
@@ -0,0 +1,108 @@
+export interface Slide {
+    image: string;
+    title: string;
+    description: string;
+    link: string;
+}
+
+export const slides: Slide[] = [
+    {
+        image: "/images/is11.webp",
+        title: "The Future of Customer Experience:How Geekonomy Helps Brands Connect with Customers in an Omnichannel World",
+        description:
+            "Consumers can move easily between online and offline interactions with businesses. Brands today are tasked with building integrated and seamless experiences. In 2025, it will be essential to tap into every interaction – be it digital, social, physical, or inbound – to create a cohesive and memorable customer experience.",
+        link: "/blog/omnichannel-marketing-2025",
+    },
+    {
+        image: "/images/is12.webp",
+        title: "White-Label PPC: Promoting Agencies to Be Smarter in Digital Marketing",
+        description:
+            "In today's rapidly evolving digital-marketing landscape, agencies are also constantly looking for ways to optimize the results of their marketing campaigns, increase their ROI and expand the services they can offer online-marketing capabilities, without overloading their in-house marketer teams.",
+        link: "/blog/white-label-ppc-digital-marketing",
+    },
+    {
+        image: "/images/is4.webp",
+        title: "The Future of E-Commerce: Geekonomys Strategies for Online Success",
+        description:
+            "E-commerce has undergone significant changes in recent years, driven by advancements in technology and changing consumer behavior. As we look ahead to the future of e-commerce, several trends are shaping the industry and presenting new opportunities for online retailers.",
+        link: "/blog/ecommerce-trends-shaping-2025",
+    },
+    {
+        image: "/images/is8.webp",
+        title: " How Geekonomy Designs Stunning Logos that Stand Out in a Crowded Market",
+        description:
+            "A logo is a critical aspect of branding, as it serves as the visual representation of a brand and helps to create a memorable and recognizable identity. In todays crowded market, where consumers are bombarded with advertisements and marketing messages, having a standout logo is essential for setting your brand apart from the competition",
+        link: "/blog/stunning-logos-design-guide",
+    },
+    {
+        image: "/images/is1.webp",
+        title:
+            "The Power of Brand Storytelling: How Geekonomy Helps Brands Connect with Customers",
+        description:
+            "Brand storytelling is a powerful tool that can help businesses connect with their audience on a deeper level. In today's competitive market, where consumers are bombarded with advertisements and marketing messages, storytelling allows brands to stand out by creating a memorable and emotional connection with their customers.",
+        link: "/blog/the-power-of-brand-storytelling",
+    },
+    {
+        image: "/images/is5.webp",
+        title:
+            "Content Marketing in 2025: Geekonomys Tips for Creating Engaging Content",
+        description:
+            "Content marketing continues to be a powerful tool for brands looking to engage with their audience and drive traffic to their website. In 2025, several trends are shaping the world of content marketing, and staying ahead of these trends is essential for creating content that resonates with your target audience.",
+        link: "/blog/content-marketing-2025",
+    },
+    {
+        image: "/images/is9.webp",
+        title: "Digital Advertising Strategies: Geekonomys Approach to Maximizing ROI",
+        description:
+            "Digital advertising is a powerful tool for reaching a targeted audience and driving conversions. In todays digital landscape, where consumers are spending more time online than ever before, having an effective digital advertising strategy is essential for businesses looking to succeed online",
+        link: "/blog/digital-marketing-strategies-that-convert",
+    },
+    {
+        image: "/images/is2.webp",
+        title:
+            "UX Design Trends for 2025: Geekonomys Guide to Creating User-Centric Experiences",
+        description:
+            "User experience (UX) design is a critical aspect of creating a successful digital product. In 2025, several trends are shaping the field of UX design, and staying ahead of these trends is essential for creating user-centric experiences that drive engagement and conversions.",
+        link: "/blog/ux-design-principles-higher-conversions",
+    },
+    {
+        image: "/images/is6.webp",
+        title: "Social Media Marketing Trends to Watch: Geekonomys Insights for Effective Campaigns",
+        description:
+            "Social media marketing is a constantly evolving field, with new trends and technologies shaping the way that brands engage with their audience. In 2025, several trends are shaping the world of social media marketing, and staying ahead of these trends is essential for creating effective campaigns that drive engagement and conversions",
+        link: "/blog/customer-retention-building-loyalty",
+    },
+    {
+        image: "/images/is10.webp",
+        title: "The Role of AI in Marketing: Geekonomys Guide to Leveraging Technology for Success",
+        description:
+            "Artificial intelligence (AI) is transforming the field of marketing, enabling businesses to analyze data, automate tasks, and personalize customer experiences at scale. In todays competitive market, where consumers expect personalized and relevant interactions with brands, AI has become an indispensable tool for marketers looking to succeed",
+        link: "/blog/future-of-ai-in-business",
+    },
+    {
+        image: "/images/is3.webp",
+        title:
+            "Why Responsive Web Design is Essential: Geekonomys Approach to Mobile Optimization",
+        description:
+            "With the increasing use of mobile devices to access the Internet, having a responsive web design is essential for ensuring that your website is accessible and user-friendly on all devices.",
+        link: "/blog/mobile-first-development-2025",
+    },
+
+
+
+
+
+
+    {
+        image: "/images/is7.webp",
+        title: "The Importance of SEO in Todays Digital Landscape: Geekonomys Best Practices",
+        description:
+            "Search engine optimization (SEO) is a critical component of any digital marketing strategy, as it helps to improve a websites visibility in search engine results pages (SERP’s) and drive organic traffic. In todays digital landscape, where consumers are increasingly turning to search engines to find products and services, having a strong SEO strategy is essential for businesses looking to succeed online",
+        link: "/blog/data-analytics-turning-information-into-insights",
+    },
+
+
+
+
+
+];
